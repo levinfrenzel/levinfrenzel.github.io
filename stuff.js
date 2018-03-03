8b15ea39cc56b862f9b9ddcb705e2bf0e7956da2
levinfrenzel <37013422+levinfrenzel@users.noreply.github.com>
2018-03-03 13:40:46 +0100

    Update stuff.js

--- stuff.js
+++ stuff.js
@@ -7,3 +7,7 @@ function closeNav() {
     document.getElementById("nav").style.height = "0%";
 }
 
+$(document).keyup(function(e) {
+  if (e.keyCode == 27) {document.getElementById("nav").style.height = "0%";
+                       }   // esc
+});
